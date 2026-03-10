3e534a588de1e6e492d21df2865b82e631c849aa
divandoviana-wq <divandoviana@gmail.com>
2026-03-09 23:51:47 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const { Telegraf } = require("telegraf");
 
-const bot = new Telegraf(process.env.BOT_8727782812:AAE-j71DC0bvWg_hb7rgoTCNuo-J23cYOIg);
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
 // ===============================
 // CONFIGURAÇÃO
